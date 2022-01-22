e4e326d6264eb2bcf5ea70b36437df955f011f85
learningst11 <learningst11@gmail.com>
2022-01-22 20:38:54 +0900

    update

---
+++ js/common.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded",
+function(){
+    console.log("로딩완료");
+
+    //1.햄버거 버튼 클릭시
+    //대상:.ham
+    document.querySelector(".ham").onclick=
+    function(){
+        console.log("햄버거");
+
+        var tg=document.querySelector("#mobx");
+        console.log("디스플레이:",window.getComputedStyle(tg).display);
+        
+        if(window.getComputedStyle(tg).display=="none"){
+            tg.style.display="block";
+        }
+
+        else{
+            tg.style.display="none";
+        }
+
+    };////////click///////////////
+});
+
