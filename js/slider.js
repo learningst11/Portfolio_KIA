e57bb7ccf03bb968d38f2c4b38a1326018eb7477
learningst11 <learningst11@gmail.com>
2022-02-08 04:52:19 +0900

    update

--- js/slider.js
+++ js/slider.js
@@ -26,7 +26,7 @@ $(() => {
     $('.slide-prev').click(function () {
        if (currentVideo == 2) {
             $('.slide-container').css('transform', 'translateX(+0vw)');
-            $('#enter').html(`<a href="./sub2.html">
+            $('#enter').html(`<a href="./sub3.html">
             <span>
             궁금한 그곳으로, 나를 Discover your world The all-new Sportage &gt;</span>
         </a>
@@ -34,7 +34,7 @@ $(() => {
             currentVideo = currentVideo - 1;
         } else if (currentVideo == 3) {
             $('.slide-container').css('transform', 'translateX(-100vw)');
-            $('#enter').html(`<a href="./sub3.html">
+            $('#enter').html(`<a href="./sub1.html">
         <span>
         내일을 위한 여정의 시작, The Kia EV6 &gt;</span>
     </a>
